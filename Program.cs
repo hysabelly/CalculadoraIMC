a423eab3751fbbf77a5aaec14d99cd03d854e0df
hysabelly <hysabellynunes@gmail.com>
2022-03-29 14:31:49 -0300

    adicionado recebimento de dados pessoais e cálculo de imc

--- Program.cs
+++ Program.cs
@@ -1,12 +1,24 @@
 ﻿using System;
 
-namespace CalculadoraIMC
+namespace EstudoDotNet
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Digite seu Nome:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o seu peso em kg:");
+            float  peso= Convert.ToSingle(Console.ReadLine());
+
+            Console.WriteLine("Digite a sua altura em metros:");
+            float  altura= Convert.ToSingle (Console.ReadLine());
+
+            double imc = peso/(altura*altura);
+
+            Console.WriteLine("{0}, a sua altura é {1} e seu peso é {2}", nome, altura, peso );
+            Console.WriteLine("O seu IMC é {0}", imc);
         }
     }
 }
